5239264b470f65bea6d2d2b13e534b36892f6b81
Burton Arceneaux <btarceneaux@gmail.com>
2022-12-14 09:27:25 -0800

    Finished the transaction look and feel for the Savings and Checking account detail page.

--- src/app/savings-details/savings-details.component.ts
+++ src/app/savings-details/savings-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SavingsAccount } from '../user-home/savings-account';
+import { Transaction } from '../user-home/transaction';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-savings-details',
@@ -7,9 +10,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SavingsDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: UserService) { }
+  loginId:string = "";
+  accountId:string = ""
+  savingsAccount:SavingsAccount = new SavingsAccount(0);
+  transactions: Transaction[] = []
 
-  ngOnInit(): void {
+  ngOnInit(): void 
+  {
+    let obj = sessionStorage.getItem("myUserId");
+    if(obj!=null)
+    {
+      this.loginId = obj;
+    }
+    else
+    {
+      console.log("ID is " + obj);
+    }
+
+    this.service.getSavingsAccountInfo(this.loginId).subscribe
+    (result=>
+      {
+        if(result.id! > 0)
+        {
+          this.savingsAccount = result;
+        }
+        else
+        {
+          console.log("No checking account was returned!");
+        }
+      },
+      error=> console.log(error),
+      ()=> //Lastly call the api to get the transaction information 
+      {
+        this.accountId = this.savingsAccount.id!.toString();
+
+        this.service.getTransactionByAccountId(this.accountId).subscribe
+        (result =>
+          {
+            if(result.length > 0)
+            {
+              this.transactions = result;
+            }
+            else
+            {
+              console.log("No transactions have been made.");
+            }
+          },
+          error=> console.log(error),
+          ()=> console.log("Transactions have been loaded.")
+        )
+      }
+    )
   }
 
 }
